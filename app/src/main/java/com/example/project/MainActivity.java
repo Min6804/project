e65cb11c65c2a9c6d0467613644972f55b18678c
min680 <jeongmin6804@gmail.com>
2024-01-17 00:17:50 +0900

    pagebase

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,77 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.removebg_pr1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                campage();
+            }
+        });
+
+        findViewById(R.id.removebg_pr2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hopage();
+            }
+        });
+
+        findViewById(R.id.removebg_pr3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playpage();
+            }
+        });
+
+        findViewById(R.id.removebg_pr4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                propage();
+            }
+        });
+
+        findViewById(R.id.removebg_pr5).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reccpage();
+            }
+        });
+
+        findViewById(R.id.removebg_pr6).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ripage();
+            }
+        });
+    }
+
+    private void campage() {
+        Intent intent = new Intent(this, CameraActivity.class);
+        startActivity(intent);
+    }
+
+    private void hopage() {
+        Intent intent = new Intent(this, VaccinationActivity.class);
+        startActivity(intent);
+    }
+
+    private void playpage() {
+        Intent intent = new Intent(this, PlayActivity.class);
+        startActivity(intent);
+    }
+
+    private void ripage() {
+        Intent intent = new Intent(this, FoodActivity.class);
+        startActivity(intent);
+    }
+
+    private void reccpage() {
+        Intent intent = new Intent(this, RecordActivity.class);
+        startActivity(intent);
+    }
+
+    private void propage() {
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
     }
 }
